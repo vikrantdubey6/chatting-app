a6e0a32685928eebed11227bd7e599c37cbb3be8
vikrant dubey <Vikrant2003aug06@gmail.com>
2026-01-06 23:37:59 +0530

    login logout handled

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -37,7 +37,7 @@ import jwt from "jsonwebtoken";
             fullName,
             profilePic: randomAvator,
         })
-
+        //USER BANANA HAI STREAM KE THROUGH
         const token = jwt.sign({userId: newUser._id},process.env.JWT_SECRET_KEY,{expiresIn: "7d"})
 
 
@@ -57,9 +57,49 @@ import jwt from "jsonwebtoken";
 }
 
 export async function login (req, res) {
-    res.send("login Route")
+   try {
+
+    const {email, password} = req.body;
+    if(!email || !password){
+        return res
+        .status(400)
+        .json({message: "All fields are required"})
+    }
+
+    const user = await User.findOne({email});
+    if(!user){
+        return res
+        .status(401)
+        .json({message: "Invalid Email or Password"})
+    }
+
+    const isPasswordCorrect = await user.matchPassword(password);
+    if(!isPasswordCorrect){
+        return res
+        .status(401)
+        .json({message: "Invalid email or password"});
+    }
+
+     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true, // prevent XSS attacks,
+      sameSite: "strict", // prevent CSRF attacks
+      secure: process.env.NODE_ENV === "production",
+    });
+
+
+    res.status(200).json({ success: true, user });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
 export async function logout (req, res) {
-    res.send("Logout Route")
+   res.clearCookie("jwt");
+  res.status(200).json({ success: true, message: "Logout successful" });
 }
