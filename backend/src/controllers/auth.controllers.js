7fa129883f940d0b0526587f4883aadf2568a356
vikrant dubey <Vikrant2003aug06@gmail.com>
2026-01-06 22:06:27 +0530

    basic setup

---
+++ backend/src/controllers/auth.controllers.js
@@ -0,0 +1,11 @@
+export async function signup (req, res) {
+    res.send("signUp Route")
+}
+
+export async function login (req, res) {
+    res.send("login Route")
+}
+
+export async function logout (req, res) {
+    res.send("Logout Route")
+}
