d95e866c76fc12a6ecc1afce925452a43e3c14e4
vikrant dubey <Vikrant2003aug06@gmail.com>
2026-01-07 03:46:50 +0530

    full user routes

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { protectRoute } from "../middleware/auth.middleware";
-import { getMyFriends, getRecommendedUsers } from "../controllers/user.controller";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { acceptFriendRequest, getFriendsRequests, getMyFriends, getOutgoingFriendReqs, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
 
 const router = express.Router()
 
@@ -9,6 +9,10 @@ router.use(protectRoute) //applying middleware to all routes
 router.get("/", getRecommendedUsers);
 router.get("/friends", getMyFriends);
 
+router.post("/friend-request/:id", sendFriendRequest)
+router.put("/friend-request/:id/accept", acceptFriendRequest)
 
+router.get("/friend-requests", getFriendsRequests)
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
 
 export default router
