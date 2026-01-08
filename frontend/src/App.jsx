afe3a805db9a7fe1e0102798d89887fb86c80ba2
vikrant dubey <Vikrant2003aug06@gmail.com>
2026-01-08 22:08:20 +0530

    video calling and chat handled

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,7 +38,13 @@ const App = () => {
 
         <Route path="/chat/:id" element={isAuthenticated && isOnboarded ? ( <Layout showSidebar={false}> <ChatPage /> </Layout>) : ( <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} /> )} />
 
-        <Route path="/call" element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />} />
+        <Route path="/call/:id" element={
+            isAuthenticated && isOnboarded ? (
+              <CallPage />
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          } />
 
         <Route path="/onboarding" element={isAuthenticated ? <OnboardingPage /> : <Navigate to="/login" />} />
 
